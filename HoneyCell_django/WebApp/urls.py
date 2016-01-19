c89fc600d30bb70421772d078790793de005c58b
luojianhe1992 <luojianhe1992@gmail.com>
2016-01-19 21:23:55 +0800

    First commit about HoneyCell_django project.

---
+++ HoneyCell_django/WebApp/urls.py
@@ -0,0 +1,42 @@
+__author__ = 'jianheluo'
+
+from django.conf.urls import include, url
+from . import views
+
+
+urlpatterns = [
+
+    # empty url
+    url(r'^$', 'WebApp.views.index', name='index'),
+
+    # new argument 'template_name'
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'WebApp/login.html'}, name='login'),
+
+    # url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', {'template_name': 'login.html'}),
+    url(r'^logout/$', 'WebApp.views.my_logout', name='logout'),
+
+    # registration is normal route
+    url(r'^registration/$', 'WebApp.views.registration', name='registration'),
+
+    # after login, show the message page to the user
+    url(r'^message/$', 'WebApp.views.message', name='message'),
+
+    # go to upload page
+    url(r'^upload/$', 'WebApp.views.upload', name='upload'),
+
+    # go to preprocess page
+    url(r'preprocess/$', 'WebApp.views.preprocess', name='preprocess'),
+
+    # go to visualization page
+    url(r'visualization/$', 'WebApp.views.visualization', name='visualization'),
+
+    # go to honeycell page
+    url(r'honeycell/$', 'WebApp.views.honeycell', name='honeycell'),
+
+    # go to honeycomb page
+    url(r'honeycomb/$', 'WebApp.views.honeycomb', name='honeycomb'),
+
+    # go to analytics page
+    url(r'analytics/$', 'WebApp.views.analytics', name='analytics'),
+
+]
