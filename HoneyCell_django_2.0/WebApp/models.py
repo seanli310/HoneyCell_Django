ffa555aea582065f7758d9cc4cdb6082ee063fcb
rsun07 <rsun2014@gmail.com>
2016-02-22 09:38:47 -0500

    adding models first time

--- HoneyCell_django_2.0/WebApp/models.py
+++ HoneyCell_django_2.0/WebApp/models.py
@@ -26,3 +26,31 @@ class Document(models.Model):
     label = models.CharField(max_length=100, blank=True)
     docfile = models.FileField(upload_to=generate_filename)
     date = models.DateTimeField(default=timezone.now)
+
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    user_name = models.CharField(max_length=18)
+    display_name = models.CharField(max_length=18)
+    user_email = models.CharField(max_length=100)
+
+class Task(models.Model):
+    task_id = models.AutoField(primary_key=True, blank=True)
+    user_id = models.ForeignKey(
+            'User.user_id',
+            on_delete=models.CASCADE,
+    )
+    TASK_IMPORTANCE = (
+        ('0', 'Important'),
+        ('1', 'Warning'),
+        ('2', 'Information'),
+    )
+    task_name = models.CharField(max_length=100)
+    task_description = models.TextField(max_length=200)
+    creat_time = models.DateTimeField(default=timezone.now)
+    finish_time = models.DateTimeField(default=timezone.now)
+    input_file_address = models.CharField(max_length=100)
+    output_file_address = models.CharField(max_length=100)
+
+
+
+
