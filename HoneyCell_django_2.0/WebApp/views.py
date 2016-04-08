0938fab5ae20f0af162c7392ee2bb89b28e8a4c2
rsun07 <rsun2014@gmail.com>
2016-04-07 22:45:15 -0400

    update request address

--- HoneyCell_django_2.0/WebApp/views.py
+++ HoneyCell_django_2.0/WebApp/views.py
@@ -396,12 +396,17 @@ def create_new_task(request):
     # return HttpResponseRedirect(reverse('newTask'))
 
     backend_url = 'http://128.2.7.38:32768/'
-    address_prefix = '/home/honeycomb/DEMODAY/honeycell/HoneyCell_Django/HoneyCell_django_2.0/media/'
-    tranining_address = address_prefix+str(training_docfile)
-    testing_address = address_prefix+str(testing_docfile)
+    address_prefix = '/home/honeycomb/DEMODAY/honeycell/HoneyCell_Django/HoneyCell_django_2.0/media/documents/' + str(context['user']) + '/' + str(context['task_folder']) + '/'
+    tranining_address = address_prefix+'tranings/'+str(training_docfile)
+    testing_address = address_prefix+'testing/'+str(testing_docfile)
+
+    print tranining_address
+    print str(training_docfile)
+    print ''
+
     my_json = {'task_id':new_task_instance.id, 'train_address': tranining_address, 'test_address': testing_address}
-    r_call_backend = requests.post(backend_url, data=my_json)
-    print(r_call_backend.content)
+    # r_call_backend = requests.post(backend_url, data=my_json)
+    # print(r_call_backend.content)
 
 
     return HttpResponseRedirect(reverse('taskDetail', kwargs={'task_id': new_task_instance.id}))
